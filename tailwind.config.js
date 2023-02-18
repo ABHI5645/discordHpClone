54533c92b29e8ebb5623622a75dfe9caa3d33ecd
Abhi <abhi24.8896@gmail.com>
2023-02-19 02:46:42 +0530

    navigationbar

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./homepage/**/*.{html,js}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      colors:{
+        primary:"#404eed",
+        black:"#23272a"
+
+      },
+    },
+  },
+  plugins: [],
+}
